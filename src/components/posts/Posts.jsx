141d0743e3a66eba57c408df38604a93ebfb9725
MoisesNer <StarCommand@Moisess-iMac.local>
2021-08-25 14:01:30 -0700

    fixed links home

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,36 +1,36 @@
 import React from 'react'
 import "./posts.css"
-import{ Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 import { withProjectConsumer } from '../../context'
 
 
-function ApocalypseContainer({context}) {
+function ApocalypseContainer({ context }) {
 
     const { projects } = context;
 
     return (
         <div className="posts">
-            {projects.map((item, index) =>(
-            <Link to={item.slug} key={index} className='post'>
-            <div className='post' key={index}>
-            <img
-                className='postImg'
-                src={item.image}
-                alt={item.alt} />
-            <div className="postInfo">
-                <div className="postCats">
-                    <span className="postCat">Cost: ${item.cost}</span>
-                    <span className="postCat">Field: {item.field}</span>
-                </div>
-                <span className="postTitle">{item.title}.</span>
-                <hr />
-                <span className="postDate">Pax: {item.pax}</span>
-            </div>
-            <p className="postDescription"> {item.description}</p>
-            {/* <Link className='btnGlobal' to={item.slug}>See Details</Link> */}
-        </div>
-        </Link>
+            {projects.map((item, index) => (
+                <Link to={item.slug} key={index} className='post'>
+                    <div className='post' key={index}>
+                        <img
+                            className='postImg'
+                            src={item.image}
+                            alt={item.alt} />
+                        <div className="postInfo">
+                            <div className="postCats">
+                                <span className="postCat">Cost: ${item.cost}</span>
+                                <span className="postCat">Field: {item.field}</span>
+                            </div>
+                            <span className="postTitle">{item.title}.</span>
+                            <hr />
+                            <span className="postDate">Pax: {item.pax}</span>
+                        </div>
+                        <p className="postDescription"> {item.description}</p>
+                        <Link className='btnGlobal' to={item.slug}>See Details</Link>
+                    </div>
+                </Link>
             ))}
         </div>
     )
